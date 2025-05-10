9b5841cf79add778795f0af300c90e521ef33517
Danilo Almeida <cs.daniloalmeida@gmail.com>
2025-05-10 15:32:50 -0300

    feat(tui): improve tui
    - show files waiting to be converted
    - move file list handling logic to _FilesView
    - organize code

--- src/tomp3/tui.py
+++ src/tomp3/tui.py
@@ -1,8 +1,10 @@
+import shutil
 import threading
 import time
 from collections import defaultdict
 from enum import Enum
 from pathlib import Path
+from typing import Optional
 
 from rich.align import Align
 from rich.console import Console, Group
@@ -13,78 +15,125 @@ from rich.text import Text
 
 
 class FileStatus(Enum):
-    CONVERTING = 1
-    CONVERTED = 2
-    ERROR = 3
+    WAITING = 1
+    CONVERTING = 2
+    CONVERTED = 3
+    ERROR = 4
 
+
+FileListType = list[tuple[Path, FileStatus]]
+ReportType = dict[FileStatus, list[Path]]
+
+
+class _FilesView:
+
+    SORTING_PRIORITY = {
+        FileStatus.WAITING: 3,
+        FileStatus.CONVERTING: 1,
+        FileStatus.CONVERTED: 2,
+        FileStatus.ERROR: 2
+    }
+
+    def __init__(self, files: list[Path], visible: int) -> None:
+        self._files = [(f, FileStatus.WAITING) for f in files]
+        self._visible = visible
+        self._lock = threading.RLock()
+        self.total = len(files)
+        self.remaining = len(files)
+    
+    def update_file_status(self, fpath: Path, status: FileStatus) -> None:
+        self._lock.acquire()
+        idx = -1
+        for i, (_fpath, _) in enumerate(self._files):
+            if _fpath == fpath:
+                idx = i
+                break
+        
+        if idx == -1:
+            raise ValueError(f"File {fpath} not found in the list.")
+        
+        self._files.pop(idx)
+        self._files.insert(0, (fpath, status))
+
+        if status == FileStatus.CONVERTED or status == FileStatus.ERROR:
+            self.remaining -= 1
+        
+        self._sort_visible()
+        self._lock.release()
+    
+    def get_visible(self) -> FileListType:
+        return self._files[:self._visible]
+    
+    def get_status(self) -> tuple[int, int]:
+        return self.total, self.remaining
+    
+    def get_report(self) -> ReportType:
+        d = defaultdict(list)
+        for (fpath, status) in self._files:
+            d[status].append(fpath)
+        return dict(d)
+    
+    def _sort_visible(self) -> None:
+        with self._lock:
+            self._files[:self._visible] = sorted(
+                self._files[:self._visible],
+                key=lambda x: self.SORTING_PRIORITY[x[1]]
+            )
+    
 
 class ConversionUI:
-    def __init__(self, visible_limit: int = 7) -> None:
-        self.console = Console()
-        self.visible_limit = visible_limit
-
-        self._converting_files: list[tuple[Path, FileStatus]] = []
-        self._lock = threading.RLock()  # For file list
-        self._running = True
-        self._total_files = 0
-        self._remaining_files = 0
-
-        self._live = Live(self._render_view(), refresh_per_second=2, screen=True)
-
-        self._thread = threading.Thread(target=self._run_live_loop, daemon=True)
-        self._thread.start()
+    def __init__(self, visible_files: int = 20) -> None:
+        self._file_view = _FilesView([], visible_files)
+        self._console = Console()
+        self._visible_files = visible_files
+        self._start()
     
-    def set_total_files(self, total_files: int) -> None:
-        self._total_files = total_files
-        self._remaining_files = total_files
-
-    def add_file(self, fpath: Path) -> None:
-        with self._lock:
-            self._converting_files.append((fpath, FileStatus.CONVERTING))
-
-    def update_file_status(self, fpath_to_update: Path, status: FileStatus) -> None:
-        with self._lock:
-            found = False
-            for i, (fpath, _) in enumerate(self._converting_files):
-                if fpath == fpath_to_update:
-                    self._converting_files[i] = (fpath, status)
-                    found = True
-                    break
-                
-            if not found:
-                raise ValueError(f"File {fpath_to_update} not found in the list.")
-            
-            if status == FileStatus.CONVERTED or status == FileStatus.ERROR:
-                self._remaining_files -= 1
-
-            self._sort_converting_files()
-
-    def stop(self) -> dict[FileStatus, list[Path]]:
+    def stop(self) -> Optional[ReportType]:
         self._running = False
         self._live.stop()
-        return self._get_report()
+        return self._file_view.get_report()
+
+    def set_file_list(self, fpaths: list[Path]) -> None:
+        self._file_view = _FilesView(fpaths, self._visible_files)
+    
+    def update_file_status(self, fname: Path, status: FileStatus) -> None:
+        return self._file_view.update_file_status(fname, status)
+
+    def _start(self) -> None:
+        self._running = True
+        self._live = Live(self._render_view(), refresh_per_second=2, screen=False)
+        self._thread = threading.Thread(target=self._run_live_loop, daemon=True)
+        self._thread.start()
 
     def _render_view(self) -> Panel:
-        with self._lock:
-            latest = self._converting_files[-self.visible_limit:]
-            items: list[ Text | Spinner ] = []
-            for fpath, status in latest:
-                filename = fpath.name
-                match status:
-                    case FileStatus.CONVERTED:
-                        items.append(Text(f"✓ {filename}", style="green"))
-                    case FileStatus.CONVERTING:
-                        items.append(Spinner("dots", text=f"Converting {filename}",
-                                       style="green"))
-                    case FileStatus.ERROR:
-                        items.append(Text(f"✗ {filename}", style="red"))
+        items: list[ Text | Spinner ] = []
+        total, remaining = 0, 0
 
-        content = Align.center(Group(*items), vertical="middle")
+        for fpath, status in self._file_view.get_visible():
+            filename = fpath.name
+            match status:
+                case FileStatus.WAITING:
+                    items.append(Text(f"• {filename}", style="dim"))
+                case FileStatus.CONVERTED:
+                    items.append(Text(f"✓ {filename}", style="green"))
+                case FileStatus.CONVERTING:
+                    items.append(Spinner("dots", text=f"{filename}", style="green"))
+                case FileStatus.ERROR:
+                    items.append(Text(f"✗ {filename}", style="red"))
+        total, remaining = self._file_view.get_status()
+
+        cols, _ = shutil.get_terminal_size()
+        content = (
+            Align.center(Group(*items[::-1]), vertical="middle")
+            if cols > 95
+            else Group(*items[::-1])
+        )
+        
         return Panel(
-            content,
+            content ,
             title="Current Conversions",
             border_style="cyan",
-            subtitle=f"Total: {self._total_files} | Remaining: {self._remaining_files}",
+            subtitle=f"Total: {total} | Remaining: {remaining}",
         )
 
     def _run_live_loop(self) -> None:
@@ -92,15 +141,3 @@ class ConversionUI:
             while self._running:
                 self._live.update(self._render_view())
                 time.sleep(0.5)
-
-    def _sort_converting_files(self) -> None:
-        with self._lock:
-            self._converting_files.sort(
-                key=lambda x: x[1] == FileStatus.CONVERTING,
-            )
-
-    def _get_report(self) -> dict[FileStatus, list[Path]]:
-        d = defaultdict(list)
-        for (fpath, status) in self._converting_files:
-            d[status].append(fpath)
-        return dict(d)
